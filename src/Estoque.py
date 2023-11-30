4bb9df62d777b27c80a22ee7d74276c8dfa5b659
Lucas Felipe <lucasfs1007@gmail.com>
2023-11-30 08:17:32 -0300

    adiciona base da classe de estoque

--- src/Estoque.py
+++ src/Estoque.py
@@ -1,5 +1,23 @@
 from dataclasses import dataclass
+from Produto import Produto
+from typing import List, Union
 
-
-@dataclass
 class Estoque:
+    def __init__(self):
+        self.produtos: List[Produto] = []
+
+    def adicionar_produto(self, produto: Produto):
+        self.produtos.append(produto)
+
+    def buscar_produto_por_nome(self, nome: str) -> Union[Produto, None]:
+        for produto in self.produtos:
+            if produto.nome == nome:
+                return produto
+        return None
+
+    def buscar_produto_por_codigo_de_barras(self, codigo: str) -> Union[Produto, None]:
+        for produto in self.produtos:
+            if produto.codigo_de_barras == codigo:
+                return produto
+        return None
+
