e8622601df824dc96ae7e8ff06baba86e5e7ec8a
pedroblome <joaopedroblome78@gmail.com>
2023-12-02 12:37:53 -0300

    adcionando Transação.py e nova exceção

--- src/Transacao.py
+++ src/Transacao.py
@@ -1,5 +1,36 @@
 from dataclasses import dataclass
+from Produto import produto
+
+class TipoTransacao(Enum):
+    RECEBIMENTO = 'RECEBIMENTO'
+    VENDA = 'VENDA'
+    DEVOLUCAO = 'DEVOLUCAO'
+    TRANSFERENCIA = 'TRANSFERENCIA'
 
 
 @dataclass
 class Transacao:
+    produto: Produto
+    tipo: TipoTransacao
+    origem: str
+    destino: str
+
+def __init__(
+        self,
+        produto: Produto,
+        tipo: TipoTransacao,
+        origem: str,
+        destino: float
+    ):
+        if not produto:
+            raise ObjetoNuloException('Produto não pode ser nulo')
+        self.produto = produto
+        if not tipo:
+            raise CampoEmBrancoException('Tipo nao pode ser nulo')
+        self.tipo = tipo
+        if not origem:
+            raise CampoEmBrancoException('Origem não pode ser vazio')
+        self.origem = origem
+        if not destino:
+            raise CampoEmBrancoException('Destino não pode ser vazio')
+        self.destino = destino
--- src/exceptions.py
+++ src/exceptions.py
@@ -1,6 +1,8 @@
 class CampoEmBrancoException(Exception):
     pass
 
+class ObjetoNuloException(Exception):
+    pass
 
 class ValorInvalidoException(Exception):
     pass
